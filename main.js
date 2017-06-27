0a14ddb2e1b798a4ccf029e173b1310c51d92bbe
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2017-06-27 23:21:33 +0100

    Making progress

--- main.js
+++ main.js
@@ -5,36 +5,33 @@ window.addEventListener('load', function () {
   /*
    * DOM elements. 
    */
-  var minutesElement = document.getElementById('minutes');
-  var secondsElement = document.getElementById('seconds');
-  var pauseButton = document.getElementById('pause');
-  var resumeButton = document.getElementById('resume');
+  var breakLengthElement = document.getElementById('break-length');
+  var sessionLengthElement = document.getElementById('session-length');
+  var remainingTimeElement = document.getElementById('remaining-time');
+  var modeElement = document.getElementById('mode');
 
   /*
-   * Note: time calculations are made using milliseconds throughout.
+   * All times are stored in milliseconds unless stated otherwise.
    */
-
-  var breakLength = 300000; // 5 minutes
-  var sessionLength = 1500000; // 25 minutes
+  var breakLengthInMinutes = 5;
+  var sessionLengthInMinutes = 25;
   var startTime;
   var mode = 'session'; // session or break
-  var currentTimerLength = sessionLength;
+  var currentTimerLength = sessionLengthInMinutes * 60000;
   var isStopped = true;
   var timerIntervalId;
 
   /*
-   * Initialise DOM elements
+   * Initialise DOM elements.
    */
-  minutesElement.innerText = '25';
-  secondsElement.innerText = '00';
+  breakLengthElement.innerText = breakLengthInMinutes;
+  sessionLengthElement.innerText = sessionLengthInMinutes;
+  remainingTimeElement.innerText = '0:00';
+  modeElement.innerText = 'Pomodoro';
 
-  // METHODS
-  // start
-  // stop
-  // clear
-  // decrease/increase break length
-  // decrease/increase session length
-
+  /*
+   * Main function to do all calculations and countdown.
+   */
   function updateTime() {
     var currentTimeInMs = Date.now();
     var elapsedTime = currentTimeInMs - startTime;
@@ -43,21 +40,33 @@ window.addEventListener('load', function () {
       if (mode === 'session') {
         mode = 'break';
         startTime = Date.now();
-        currentTimerLength = breakLength;
+        currentTimerLength = breakLengthInMinutes * 60000;
       } else if (mode === 'break') {
         mode = 'session';
         startTime = Date.now();
-        currentTimerLength = sessionLength;
+        currentTimerLength = sessionLengthInMinutes * 60000;
       }
     } else {
       var minutes = parseInt(remainingTimeInMs / 1000 / 60);
       var seconds = parseInt(remainingTimeInMs / 1000 % 60);
-      minutesElement.innerText = minutes;
-      secondsElement.innerText = seconds;
+      remainingTimeElement.innerText = minutes + ':' + seconds;
     }
   }
 
-  pauseButton.addEventListener('click', function () {
+  /*
+   * Add button event listeners.
+   */
+
+  document.getElementById('start').addEventListener('click', function () {
+    if (isStopped) {
+      isStopped = false;
+      startTime = Date.now();
+      timerIntervalId = setInterval(updateTime, 100);
+      document.body.classList.add(mode);
+    }
+  });
+
+  document.getElementById('stop').addEventListener('click', function () {
     if (!isStopped) {
       isStopped = true;
       currentTimerLength -= Date.now() - startTime;
@@ -65,12 +74,44 @@ window.addEventListener('load', function () {
     }
   });
 
-  resumeButton.addEventListener('click', function () {
-    if (isStopped) {
-      isStopped = false;
-      startTime = Date.now();
-      timerIntervalId = setInterval(updateTime, 100);
-    }
+  document.getElementById('clear').addEventListener('click', function () {
+    document.body.classList.remove('session', 'break');
   });
+
+  document.getElementById('decrease-break').addEventListener('click', function () {
+    updateBreakLength(breakLengthInMinutes - 1);
+  });
+
+  document.getElementById('increase-break').addEventListener('click', function () {
+    updateBreakLength(breakLengthInMinutes + 1);
+  });
+
+  function updateBreakLength(length) {
+    if (length < 1) return;
+    breakLengthInMinutes = length;
+    breakLengthElement.innerText = breakLengthInMinutes;
+
+    // extra functionality
+  }
+
+  document.getElementById('decrease-session').addEventListener('click', function () {
+    updateSessionLength(sessionLengthInMinutes - 1);
+  });
+
+  document.getElementById('increase-session').addEventListener('click', function () {
+    updateSessionLength(sessionLengthInMinutes + 1);
+  });
+
+  function updateSessionLength(length) {
+    if (length < 1) return;
+    sessionLengthInMinutes = length;
+    sessionLengthElement.innerText = sessionLengthInMinutes;
+
+    // look at this
+    if (mode === 'session') {
+      startTime = Date.now();
+      currentTimerLength = sessionLengthInMinutes * 60000;
+    }
+  }
 
 });
